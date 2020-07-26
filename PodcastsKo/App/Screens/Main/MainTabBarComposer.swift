cd01b4604978188d4c69fb3593f4b9e834dbf256
John Roque Jorillo <jorillojohnroque@gmail.com>
2020-07-26 09:21:13 +0800

    Add MainTabBarController

---
+++ PodcastsKo/App/Screens/Main/MainTabBarComposer.swift
@@ -0,0 +1,23 @@
+//
+//  MainTabBarComposer.swift
+//  PodcastsKo
+//
+//  Created by John Roque Jorillo on 7/26/20.
+//  Copyright © 2020 JohnRoque Inc. All rights reserved.
+//
+
+import Foundation
+
+final class MainTabBarComposer {
+    
+    private init() {}
+    
+    /// Compose MainTabBarController
+    /// - Returns: MainTabBarController
+    static func composeWith() -> MainTabBarController {
+        let vc = MainTabBarController()
+        
+        return vc
+    }
+    
+}
---
+++ PodcastsKo/App/Screens/Main/MainTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabBarController.swift
+//  PodcastsKo
+//
+//  Created by John Roque Jorillo on 7/26/20.
+//  Copyright © 2020 JohnRoque Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
