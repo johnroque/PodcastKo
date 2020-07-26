02f1062c71266598557f0c1d2898c0cd67885999
John Roque Jorillo <jorillojohnroque@gmail.com>
2020-07-26 17:10:37 +0800

    Add FavoritesViewController

---
+++ PodcastsKo/App/Screens/Favorites/FavoritesComposer.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesComposer.swift
+//  PodcastsKo
+//
+//  Created by John Roque Jorillo on 7/26/20.
+//  Copyright © 2020 JohnRoque Inc. All rights reserved.
+//
+
+import Foundation
+
+final class FavoritesComposer {
+    
+    private init() {}
+    
+    static func composeWith() -> FavoritesViewController {
+        let vc = FavoritesViewController()
+        return vc
+    }
+    
+}
---
+++ PodcastsKo/App/Screens/Favorites/FavoritesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesViewController.swift
+//  PodcastsKo
+//
+//  Created by John Roque Jorillo on 7/26/20.
+//  Copyright © 2020 JohnRoque Inc. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
