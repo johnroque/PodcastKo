0d55526d1fac143977490516d5377e562fb3393a
John Roque Jorillo <jorillojohnroque@gmail.com>
2020-07-31 03:46:29 +0800

    Add MusicPlayerService

---
+++ PodcastsKo/App/Services/MusicPlayerService.swift
@@ -0,0 +1,39 @@
+//
+//  MusicPlayerService.swift
+//  PodcastsKo
+//
+//  Created by John Roque Jorillo on 7/31/20.
+//  Copyright © 2020 JohnRoque Inc. All rights reserved.
+//
+
+import Foundation
+import AVKit
+
+protocol AudioPlayerable {
+    func setCurrent(url: URL)
+    func play()
+    func pause()
+}
+
+class MusicPlayerService: AudioPlayerable {
+    
+    let avPlayer: AVPlayer = {
+        let player = AVPlayer()
+        player.automaticallyWaitsToMinimizeStalling = false
+        return player
+    }()
+   
+    func setCurrent(url: URL) {
+        let playerItem = AVPlayerItem(url: url)
+        avPlayer.replaceCurrentItem(with: playerItem)
+    }
+    
+    func play() {
+        avPlayer.play()
+    }
+    
+    func pause() {
+        
+    }
+    
+}
