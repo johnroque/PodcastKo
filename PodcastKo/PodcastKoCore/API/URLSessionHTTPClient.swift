b973a3a4287a66e39034928ef0a58f585ec7150f
john roque jorillo <jorillojohnroque@gmail.com>
2021-05-23 00:46:06 +0800

    Add URLSessionHTTPClient

---
+++ PodcastKo/PodcastKoCore/API/URLSessionHTTPClient.swift
@@ -0,0 +1,59 @@
+//
+//  URLSessionHTTPClient.swift
+//  PodcastKoCore
+//
+//  Created by John Roque Jorillo on 5/22/21.
+//  Copyright © 2021 JohnRoque Inc. All rights reserved.
+//
+
+import Foundation
+
+public final class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+    
+    public init(session: URLSession) {
+        self.session = session
+    }
+    
+    private struct UnexpectedValuesRepresentation: Error {}
+    
+    private struct URLSessionTaskWrapper: HttpClientTask {
+        let wrapped: URLSessionTask
+        
+        func cancel() {
+            wrapped.cancel()
+        }
+    }
+    
+    public func get(request: URLSessionRequest, completionHandler: @escaping (HTTPClient.Result) -> Void) -> HttpClientTask {
+        let task = session.dataTask(with: request.urlRequest) { (data, response, error) in
+            completionHandler(Result {
+                if let error = error {
+                    throw error
+                } else if let data = data, let response = response as? HTTPURLResponse {
+                    return (data, response)
+                } else {
+                    throw UnexpectedValuesRepresentation()
+                }
+            })
+        }
+        task.resume()
+        return URLSessionTaskWrapper(wrapped: task)
+    }
+    
+    public func download(request: URLSessionRequest, progressHandler: ((Double) -> Void)?, completionHandler: @escaping (DownloadResult) -> Void) -> HttpClientTask {
+        let task = session.downloadTask(with: request.urlRequest) { (url, response, error) in
+            completionHandler(DownloadResult {
+                if let error = error {
+                    throw error
+                } else if let url = url, let response = response as? HTTPURLResponse {
+                    return (url, response)
+                } else {
+                    throw UnexpectedValuesRepresentation()
+                }
+            })
+        }
+        task.resume()
+        return URLSessionTaskWrapper(wrapped: task)
+    }
+}
