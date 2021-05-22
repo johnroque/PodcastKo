00c230b08679e0a58b450768cc02c01cf468acb8
john roque jorillo <jorillojohnroque@gmail.com>
2021-05-22 21:09:58 +0800

    Fix PodcastCore config & add podcast entity

---
+++ PodcastKo/PodcastKoCore/Domain/Entities/Podcast.swift
@@ -0,0 +1,19 @@
+//
+//  Podcast.swift
+//  PodcastKoCore
+//
+//  Created by John Roque Jorillo on 5/22/21.
+//  Copyright © 2021 JohnRoque Inc. All rights reserved.
+//
+
+import Foundation
+
+struct Podcast: Equatable {
+    
+    let trackName: String?
+    let artistName: String?
+    let artworkUrl600: String?
+    let trackCount: Int?
+    let feedUrl: String?
+    
+}
