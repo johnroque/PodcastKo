5c949cddd1fdc49f4f60f7da703050fe061ee0ca
john roque jorillo <jorillojohnroque@gmail.com>
2021-05-23 01:50:14 +0800

    Enable macOS target

---
+++ PodcastKo/PodcastKoCoreTests/API/URLSessionHTTPClientTests.swift
@@ -0,0 +1,13 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  PodcastKoCoreTests
+//
+//  Created by John Roque Jorillo on 5/23/21.
+//  Copyright © 2021 JohnRoque Inc. All rights reserved.
+//
+
+import XCTest
+
+class URLSessionHTTPClientTests: XCTestCase {
+
+}
