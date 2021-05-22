e1b2b1cad0865aaa2e0d5f63a66599fdf98a871b
john roque jorillo <jorillojohnroque@gmail.com>
2021-05-22 21:11:17 +0800

    Add Episode entity

---
+++ PodcastKo/PodcastKoCore/Domain/Entities/Episode.swift
@@ -0,0 +1,22 @@
+//
+//  Episode.swift
+//  PodcastKoCore
+//
+//  Created by John Roque Jorillo on 5/22/21.
+//  Copyright © 2021 JohnRoque Inc. All rights reserved.
+//
+
+import Foundation
+
+struct Episode: Equatable {
+    
+    let title: String?
+    let pubDate: Date?
+    let description: String?
+    let author: String?
+    let streamUrl: String?
+    let image: String?
+    
+    var fileUrl: String?
+    
+}
