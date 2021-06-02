527a814cd24a484ab9487864650472b2e1e07d5b
john roque jorillo <jorillojohnroque@gmail.com>
2021-06-03 00:38:17 +0800

    Add expect helper method

--- PodcastKo/PodcastKoCoreTests/API/Gateways/DownloadEpisodeGatewayTests.swift
+++ PodcastKo/PodcastKoCoreTests/API/Gateways/DownloadEpisodeGatewayTests.swift
@@ -32,50 +32,18 @@ class DownloadEpisodeGatewayTests: XCTestCase {
         let clientError = NSError(domain: "Test", code: 0)
         let (sut, client) = makeSUT()
         
-        let exp = expectation(description: "Wait for load completion")
-        
-        _ = sut.downloadEpisode(
-            url: anyURL(),
-            progressHandler: nil) { result in
-            
-            switch result {
-            case let .failure(error as NSError):
-                XCTAssertEqual(error, clientError)
-            default:
-                XCTFail("Expected failure, got \(result) instead.")
-            }
-            
-            exp.fulfill()
+        expect(sut, toCompleteWith: .failure(clientError)) {
+            client.complete(with: clientError)
         }
-        
-        client.complete(with: clientError)
-        
-        wait(for: [exp], timeout: 2.0)
     }
     
     func test_download_deliversDownloadURLOnDownloadRequestSucceeds() {
         let downloadedURL = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT()
         
-        let exp = expectation(description: "Wait for load completion")
-        
-        _ = sut.downloadEpisode(
-            url: anyURL(),
-            progressHandler: nil) { result in
-            
-            switch result {
-            case let .success(url):
-                XCTAssertEqual(url, downloadedURL)
-            default:
-                XCTFail("Expected result, got \(result) instead.")
-            }
-            
-            exp.fulfill()
+        expect(sut, toCompleteWith: .success(downloadedURL)) {
+            client.complete(withStatusCode: 200, url: downloadedURL)
         }
-        
-        client.complete(withStatusCode: 200, url: downloadedURL)
-        
-        wait(for: [exp], timeout: 2.0)
     }
     
     // MARK: - Helpers
@@ -85,6 +53,30 @@ class DownloadEpisodeGatewayTests: XCTestCase {
         trackForMemoryLeaks(client)
         trackForMemoryLeaks(sut)
         return (sut, client)
+    }
+    
+    private func expect(_ sut: DownloadEpisodeUseCase, toCompleteWith expectedResult: DownloadEpisodeGateway.Completion, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+        let exp = expectation(description: "Wait for load completion")
+
+        _ = sut.downloadEpisode(
+            url: anyURL(),
+            progressHandler: nil) { receivedResult in
+
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedURL), .success(expectedURL)):
+                XCTAssertEqual(receivedURL, expectedURL, file: file, line: line)
+            case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+            default:
+                XCTFail("Expected result \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+            }
+
+            exp.fulfill()
+        }
+
+        action()
+
+        wait(for: [exp], timeout: 2.0)
     }
     
     private class HTTPDownloadClientSpy: HTTPDownloadClient {
@@ -104,6 +96,21 @@ class DownloadEpisodeGatewayTests: XCTestCase {
         func download(request: URLRequest, progressHandler: ((Double) -> Void)?, completionHandler: @escaping (DownloadResult) -> Void) -> HTTPClientTask {
             messages.append((request, progressHandler, completionHandler))
             return Task()
+        }
+        
+        // MARK: - Helpers
+        
+        func complete(with error: Error, at index: Int = 0) {
+            messages[index].completion(.failure(error))
+        }
+        
+        func complete(withStatusCode code: Int, url: URL, at index: Int = 0) {
+            let response = HTTPURLResponse(url: messages[index].request.url!, // TODO: make this safe
+                                           statusCode: code,
+                                           httpVersion: nil,
+                                           headerFields: nil)
+            
+            messages[index].completion(.success((url, response!)))
         }
     }
     
